ffb5f2c04e1e56fa107847bbb751a279adb9a3ed
Migsayal <>
2023-11-08 23:54:50 -0300

    spinner en formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,36 +1,51 @@
-import './Formulario.css';
-import { useState } from 'react';
-import { validarUsuario } from './validacionUsuarios';
+import './Formulario.css'
+import { useState } from 'react'
+import { validarUsuario } from './validacionUsuarios'
+import { Spin } from 'antd'
 
 export function Formulario({ setUser }) {
-  const [nombre, setNombre] = useState('');
-  const [contrasenia, setContrasenia] = useState('');
-  const [error, setError] = useState(false);
+  const [nombre, setNombre] = useState('')
+  const [contrasenia, setContrasenia] = useState('')
+  const [error, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     if (nombre === '' || contrasenia === '') {
-      setError(true);
-      return;
+      setError(true)
+      return
     }
 
-    setError(false);
+    setError(false)
     const user = {
       email: nombre,
       password: contrasenia,
       rol: '',
       token: '',
-    };
+    }
 
     // Llama a validarUsuario y espera hasta que se complete la función
-    await validarUsuario({ user });
+    setIsLoading(true)
+    await validarUsuario({ user })
+    setIsLoading(false)
 
-    setUser(user);
-  };
+    setUser(user)
+  }
 
   return (
     <section>
+
+        <Spin
+          spinning={isLoading}
+          size="large"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        />
+   
       <h1 className="tituloLogin">Login</h1>
       <form className="formulario" onSubmit={handleSubmit}>
         <input
@@ -47,5 +62,5 @@ export function Formulario({ setUser }) {
       </form>
       {error && <p className="msg">Todos los campos son obligatorios</p>}
     </section>
-  );
+  )
 }
